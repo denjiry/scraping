747c4d585ee8ebfd74c1216a76199e5a8fd48ef0
kento tajiri <k.denjiry@gmail.com>
2018-02-16 23:32:38 +0900

    Add test_get_class_text

--- test.py
+++ test.py
@@ -1,19 +1,31 @@
 import pytest
-from scp import soup, get_html
+from bs4 import BeautifulSoup
 
 
 URL = "https://wbawakate.connpass.com/event/"
 html_file = "index.html"
+with open(html_file) as p:
+        HTML = p.read()
 
 
 def test_get_html():
+    from scp import get_html
     html = get_html(URL)
     assert isinstance(html, str), "html isn't str: {}".format(type(html))
     return html
 
 
 def test_soup():
-    with open(html_file) as p:
-        html = p.read()
-    sp = soup(html)
+    from scp import soup
+    sp = soup(HTML)
+    assert isinstance(sp, BeautifulSoup)
     return sp
+
+
+def test_get_class_text():
+    from scp import get_class_text
+    sp = BeautifulSoup(HTML, "lxml")
+    list_text = get_class_text(sp, "event_title")
+    assert isinstance(list_text, list)
+    assert isinstance(list_text[0], str)
+    return list_text
