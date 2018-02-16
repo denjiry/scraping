3a2a7e970f144840e6938b14a7dfb4d0ed76c751
kento tajiri <k.denjiry@gmail.com>
2018-02-16 11:13:32 +0900

    Add test_soup

--- scp.py
+++ scp.py
@@ -1,11 +1,20 @@
+import requests
 from bs4 import BeautifulSoup
 import sys
 
 
-def main(URL):
-    with open(URL) as p:
-        html = p.read()
+def get_html(URL):
+    requests
+    return html
+
+
+def soup(html):
     soup = BeautifulSoup(html, "lxml")
+    
+
+def main(URL):
+    html = get_html(URL)
+    soup(html)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import pytest
-from scp import main
+from scp import *
 
 
 URL = "https://wbawakate.connpass.com/event/"
+html_file = "index.html"
+
+
+def test_soup():
+    with open(html_file) as p:
+        html = p.read()
+    soup(html)
 
 
 def test_main():
