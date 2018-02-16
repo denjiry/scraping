1007166db74aa5ba9b396ff0b99a33c691f2b77a
kento tajiri <k.denjiry@gmail.com>
2018-02-16 22:25:16 +0900

    Add functions

--- scp.py
+++ scp.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-import sys
+
+
+PAGEs = ["https://wbawakate.connpass.com/event/",
+         "https://wbawakate.connpass.com/event/?page=2",
+         "https://wbawakate.connpass.com/event/?page=3"]
 
 
 def get_html(URL):
@@ -12,14 +16,39 @@ def get_html(URL):
 
 def soup(html):
     soup = BeautifulSoup(html, "lxml")
+    return soup
 
 
-def main(URL):
+def get_class_text(soup, class_):
+    list_xml = soup.find_all("p", attrs={"class": class_})
+    list_text = [xml.text for xml in list_xml]
+    return list_text
+
+
+def get_pair_page(URL):
     html = get_html(URL)
-    soup(html)
+    sp = soup(html)
+    list_date = get_class_text(sp, "schedule")
+    list_title = get_class_text(sp, "event_title")
+    return list(zip(list_date, list_title))
+
+
+def get_events(pages):
+    date_event = []
+    for url in pages:
+        pairs = get_pair_page(url)
+        date_event.append(pairs)
+    return date_event
+
+
+def dump_events(events, filename):
+    pass
+
+
+def main():
+    events = get_events(PAGEs)
+    dump_events(events, "events.txt")
 
 
 if __name__ == "__main__":
-    URL = sys.argv[1]
-    print("URL: ",URL)
-    main(URL)
+    main()
--- test.py
+++ test.py
@@ -15,8 +15,5 @@ def test_get_html():
 def test_soup():
     with open(html_file) as p:
         html = p.read()
-    soup(html)
-
-
-def test_main():
-    main(URL)
+    sp = soup(html)
+    return sp
